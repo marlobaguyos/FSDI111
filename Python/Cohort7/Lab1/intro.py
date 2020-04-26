6e3c3c88c1afd94329b0bcfaa024570b478bb2f6
Marlo <marlo.baguyos@outlook.com>
2020-04-26 12:54:28 -0400

    update

---
+++ Python/Cohort7/Lab1/intro.py
@@ -0,0 +1,35 @@
+print('Hello world')
+
+# data types
+name="Marlo"
+last="Baguyos"
+
+print(name)
+# print(average)
+# print(found)
+
+# operations
+
+print(21 + 21)
+print(100 - 50)
+print(12 * 321)
+print(100 / 10)
+print(10 % 3) # % = modulus operator (gives the residue)
+
+print(name + name)
+# print(age + age)
+# print(name + str(age))
+
+def separator():
+    print('____________________________________________')
+    print('inside')
+
+print('outside')
+separator()
+age = 45
+if(age > 90):
+    print('You are still young')
+elif (age == 90):
+    print('You are on the border line')
+else:
+    print('Sorry bud, you are getting old : ) ')

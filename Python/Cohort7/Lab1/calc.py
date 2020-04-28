7280441c76d671eb1f08cc32cbda0eff4d3fd998
Marlo <marlo.baguyos@outlook.com>
2020-04-28 09:47:25 -0400

    update

--- Python/Cohort7/Lab1/calc.py
+++ Python/Cohort7/Lab1/calc.py
@@ -2,27 +2,58 @@ def separator():
     print("-" * 30)
 
 def print_menu():
+    print('\n' * 5)
     separator()
     print('Welcome to PyCalc')
     separator()
 
     print('[1] - Add')
     print('[2] - Substract')
+    print('[3] - Multiply')
+    print('[4] - Divide')
 
-    def sum(num1, num2):
-        return num1 + num2
+    print('[x] - Close')
+
+    separator()
+
+def sum(num1, num2):
+    return num1 + num2
+
+def substract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2 if num2 else 0
 
 opc = ''
 while(opc != 'x'):
     print_menu()
     opc = input('Please select an option: ')
-    print('user choose: ' + opc)
+    print('Selected: ' + opc)
+    if(opc == 'x'):
+        break # break = finish loop here and get out of the loop
 
     num1 = float(input('First number: '))
     num2 = float(input('Second number: '))
 
-    if(opc == 1):
+    if(opc == '1'):
         res = sum(num1, num2)
+        print('Addition my good sir.')
         print("Result: " + str(res))
+    elif(opc == '2'):
+        print('You have selected Substraction my good sir.')
+        print("Result: " + str(substract(num1, num2)))
+    elif(opc == '3'):
+        print('You have selected Multiplication my good sir.')
+        print("Result: " + str(multiply(num1, num2)))
+    elif(opc == '4'):
+        print('You have selected Division my good sir.')
+        
+            
+
+    input("Press Enter to continue...")
 
 print("Byte byte!")

ee79c6fb3e71b25edffdeef40906fd5d25e89aaf
Marlo <marlo.baguyos@outlook.com>
2020-04-28 20:27:17 -0400

    update

---
+++ Python/Cohort7/WareHouse/menu.py
@@ -0,0 +1,18 @@
+def menu():
+    print("-" * 30)
+    print("Warehouse Control")
+    print("-" * 30)
+
+    print('[1] - Add')
+    print('[2] - Substract')
+    print('[3] - Multiply')
+    print('[4] - Divide')
+
+    print('[x] - Close')
+    print("-" * 30)
+
+# def clear():
+#     return os.system('cls' if os.name == 'nt' else 'clear')
+
+clear = lambda: os.system('cls')
+clear()
---
+++ Python/Cohort7/WareHouse/warehouse.py
@@ -0,0 +1,24 @@
+"""
+    Program: Warehouse management system
+    Functionality:
+        - Repeated menu
+"""
+import os
+from menu import menu, clear
+
+def separator():
+    print("-" * 30)
+
+
+
+
+
+# start menu
+
+opc = ''
+while(opc != 'x'):
+    clear()
+    menu()
+    opc = input("Please select an option: ")
+
+    input("Press enter to continue...")

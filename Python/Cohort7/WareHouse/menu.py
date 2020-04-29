3f4a68389625ad0363a48fd1760a934f3fbc2345
Marlo <marlo.baguyos@outlook.com>
2020-04-28 21:12:45 -0400

    update

--- Python/Cohort7/WareHouse/menu.py
+++ Python/Cohort7/WareHouse/menu.py
@@ -1,16 +1,16 @@
 import os
 
 def menu():
+    print('\n' * 5)
     print("-" * 30)
     print("      Warehouse Control")
     print("-" * 30)
-
     print('[1] Register Items')
-    print('[2] - Substract')
+    print('[2] Catalog')
     print('[3] - Multiply')
     print('[4] - Divide')
-
     print('[x] - Close')
+    print('\n')
     print("-" * 30)
 
 # def clear():
@@ -18,3 +18,9 @@ def menu():
 
 clear = lambda: os.system('cls')
 clear()
+
+def header(title):
+    clear()
+    print("-" * 30)
+    print(" " + title)
+    print("-" * 30)
--- Python/Cohort7/WareHouse/warehouse.py
+++ Python/Cohort7/WareHouse/warehouse.py
@@ -10,27 +10,51 @@
             stock
 """
 
-from menu import menu, clear
+from menu import menu, clear, header
+from item import Item
+
 
 # global vars
+catalog = []
+
+
 
 # functions
 def register_item():
+    header("Register new Item")
 
     title = input("New item title: ")
     cat = input("New item category: ")
-    price = input("New item price: ")
-    stock = input("New item stock: ")
+    price = float(input("New item price: "))
+    stock = int(input("New item stock: "))
 
+    new_item = Item() # <- create instances of a class (objects)
+    new_item.id = 0
+    new_item.title = title
+    new_item.category = cat
+    new_item.price = price
+    new_item.stock = stock
 
+    catalog.append(new_item)
+    print("Item created!")
+
+def display_catalog():
+    header("Current Catalog")
+
+    size = len(catalog)
+    print("there are: " + str(size) + " items")
+
+# instructions
 # start menu
-
 opc = ''
 while(opc != 'x'):
     clear()
     menu()
     opc = input("Please select an option: ")
+    print("-" * 30)
     if(opc == '1'):
         register_item()
+    elif (opc == '2'):
+        display_catalog()
 
     input("Press enter to continue...")

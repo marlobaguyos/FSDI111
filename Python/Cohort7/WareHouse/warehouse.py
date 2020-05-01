7888cd3627cc9defea602ef9b115853a3ad10d18
Marlo <marlo.baguyos@outlook.com>
2020-04-30 22:14:04 -0400

    update

--- Python/Cohort7/WareHouse/warehouse.py
+++ Python/Cohort7/WareHouse/warehouse.py
@@ -10,6 +10,20 @@
             stock
         - Display Catalog
         - Display items with no stock (out of stock)
+
+        - Saving / retrieving data to/fron file
+
+        - Update the stock of an item
+            - show the list items
+            - ask the user to choose and id
+            - ask the user for the new stock value
+            - find the item with selected id
+            - update the stock
+            - save changes
+
+        - Print the Total value of the stock (sum (price * stock))
+
+        - Remove an Item from the Catalog
 """
 
 from menu import menu, clear, header
@@ -35,11 +49,15 @@ def save_catalog():
 def read_catalog():
     try:
         global data_file
+        global last_id
         reader = open(data_file, "rb")
         temp_list = pickle.load(reader)
 
         for item in temp_list:
             catalog.append(item)
+
+        last = catalog[-1]
+        last_id = last.id
 
         how_many = len(catalog)
         print("** Loaded " + str(how_many) + " items")
@@ -88,8 +106,8 @@ def display_catalog():
         print("|" + str(item.id).rjust(3)
             + "|" + item.title.ljust(30)
             + "|" + item.category.ljust(15)
-            + "|" + str(item.price).rjust(11)
-            + "|" + str(item.stock).rjust(8) + "|")
+            + "|" + str(item.price).rjust(10)
+            + "|" + str(item.stock).rjust(5) + "|")
         print("-" * 70)
 
 
@@ -113,6 +131,35 @@ def display_no_stock():
                 + "|" + str(item.stock).rjust(8) + "|")
             print("-" * 70)
 
+def update_stock():
+    display_catalog()
+    id = int(input("Select an ID: "))
+    # find the item with id = id
+    found = False 
+    for item in catalog:
+        if(item.id == id):
+            found = True
+            stock = int(input("New stock value: "))
+            item.stock = stock
+            print('Stock updated!')
+
+    if(not found):
+        print("Error: Selected Id does not exist, try again")
+
+def cal_total_stock():
+    total = 0
+    for item in catalog:
+        total += float(item.price * item.stock)
+    print("Total Stock Value: $" + str(total))
+
+def delete_item():
+    display_catalog()
+    id = int(input("Select an ID: "))
+    for item in catalog:
+        if (id == item.id):
+            catalog.remove(item)
+            
+
 
 # instructions
 # start menu
@@ -132,5 +179,13 @@ while(opc != 'x'):
         display_catalog()
     elif (opc == '3'):
         display_no_stock()
+    elif (opc == '4'):
+        update_stock()
+        save_catalog()
+    elif (opc == '5'):
+        cal_total_stock()
+    elif (opc == '6'):
+        delete_item()
+        display_catalog()
 
     input("Press enter to continue...")
